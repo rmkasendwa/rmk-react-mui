f2d77630de3a2f5db70c1b3af24d5cfdc9b1825d
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2022-01-27 14:21:01 +0300

    #6 setsup unit test process

---
+++ src/components/AuthGuard.spec.ts
@@ -0,0 +1,8 @@
+describe('AuthGuard', () => {
+  // TODO: Remove this and implement actual test cases
+  it('should run successfully', async () => {
+    expect(true).toBe(true);
+  });
+});
+
+export {};
