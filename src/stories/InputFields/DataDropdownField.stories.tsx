65460d4a89eb9ac62daf360c3674087f2dbb63d7
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2022-02-02 05:53:51 +0300

    #50 adds docs folder

--- src/stories/InputFields/DataDropdownField.stories.tsx
+++ src/stories/InputFields/DataDropdownField.stories.tsx
@@ -10,17 +10,39 @@ export default {
   },
 } as ComponentMeta<typeof DataDropdownField>;
 
-const Template: ComponentStory<typeof DataDropdownField> = ({ required }) => {
+const Template: ComponentStory<typeof DataDropdownField> = ({
+  required,
+  label,
+  options,
+}) => {
+  const props: any = {};
+  required && (props.required = required);
+  label && (props.label = label);
+  options && (props.options = options);
+
   return (
-    <DataDropdownField
-      label="Dropdown"
-      {...{ required }}
-      sx={{ minWidth: 300 }}
-    />
+    <DataDropdownField label="Dropdown" {...props} sx={{ minWidth: 300 }} />
   );
 };
 
 export const Default = Template.bind({});
 Default.args = {
   required: true,
+};
+
+export const WithStaticOptions = Template.bind({});
+WithStaticOptions.args = {
+  label: 'Dropdown With Options',
+  required: true,
+  options: ['One', 'Two', 'Three'].map((value) => ({ label: value, value })),
+};
+
+export const WithOverflowingOptions = Template.bind({});
+WithOverflowingOptions.args = {
+  label: 'Dropdown With Many Options',
+  required: true,
+  options: Array.from({ length: 200 }).map((_, index) => ({
+    label: String(index),
+    value: index,
+  })),
 };
