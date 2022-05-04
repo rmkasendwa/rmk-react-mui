7eae6c3ebb54a9df7a3d6a07e613d69e4d065e5e
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2022-05-05 02:39:55 +0300

    #107 Updates dates utility to read formats from environment variables

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,13 +1,28 @@
 import 'datejs';
 
+const DEFAULT_DATE_FORMAT =
+  process.env.DEFAULT_DATE_FORMAT ||
+  process.env.REACT_DEFAULT_DATE_FORMAT ||
+  'dd MMM yyyy';
+
+const DEFAULT_DATE_TIME_FORMAT =
+  process.env.DEFAULT_DATE_TIME_FORMAT ||
+  process.env.REACT_DEFAULT_DATE_TIME_FORMAT ||
+  `dddd, ${DEFAULT_DATE_FORMAT} hh:mm tt`;
+
+const DEFAULT_DATE_TIME_FORMAT_WITH_SECONDS =
+  process.env.DEFAULT_DATE_TIME_FORMAT_WITH_SECONDS ||
+  process.env.REACT_DEFAULT_DATE_TIME_FORMAT_WITH_SECONDS ||
+  `dddd, ${DEFAULT_DATE_FORMAT} hh:mm:ss tt`;
+
 export const formatDate = (
   dateParam: string | number | Date,
   includeTime?: boolean | 'SECONDS'
 ) => {
   const dateFormat = (() => {
-    if (includeTime === true) return 'dddd, dd MMM yyyy hh:mm tt';
-    if (includeTime === 'SECONDS') return 'dddd, dd MMM yyyy hh:mm:ss tt';
-    return 'dd MMM yyyy';
+    if (includeTime === true) return DEFAULT_DATE_TIME_FORMAT;
+    if (includeTime === 'SECONDS') return DEFAULT_DATE_TIME_FORMAT_WITH_SECONDS;
+    return DEFAULT_DATE_FORMAT;
   })();
   if (dateParam instanceof Date) return dateParam.toString(dateFormat);
   if (typeof dateParam === 'string' && dateParam.toString().match(/^-?\d+$/))
