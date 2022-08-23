4870dfe475c33b9673102d371daad34694273bcd
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2022-08-23 21:44:24 +0300

    Refactors SearchSyncToolbar

--- src/components/SearchSyncToolbar.tsx
+++ src/components/SearchSyncToolbar.tsx
@@ -1,4 +1,10 @@
 import SearchIcon from '@mui/icons-material/Search';
+import {
+  ClickAwayListener,
+  IconButton,
+  Typography,
+  TypographyProps,
+} from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Toolbar, { ToolbarProps } from '@mui/material/Toolbar';
 import { Children, FC, ReactNode, useEffect, useState } from 'react';
@@ -7,15 +13,18 @@ import TextField from './InputFields/TextField';
 import ReloadIconButton, { IReloadIconButtonProps } from './ReloadIconButton';
 
 export interface ISearchSyncToolbarProps
-  extends ToolbarProps,
+  extends Omit<ToolbarProps, 'title'>,
     Partial<Pick<IReloadIconButtonProps, 'load' | 'loading' | 'errorMessage'>> {
+  title?: ReactNode;
   searchTerm?: string;
   searchFieldPlaceholder?: string;
   onChangeSearchTerm?: (searchTerm: string) => void;
   tools?: ReactNode | ReactNode[];
+  TitleProps?: Partial<TypographyProps>;
 }
 
 export const SearchSyncToolbar: FC<ISearchSyncToolbarProps> = ({
+  title,
   searchTerm: searchTermProp,
   searchFieldPlaceholder,
   load,
@@ -23,9 +32,12 @@ export const SearchSyncToolbar: FC<ISearchSyncToolbarProps> = ({
   errorMessage,
   onChangeSearchTerm,
   tools,
+  TitleProps = {},
   ...rest
 }) => {
+  const { sx: titlePropsSx, ...titlePropsRest } = TitleProps;
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchFieldOpen, setSearchFieldOpen] = useState(false);
 
   useEffect(() => {
     setSearchTerm(searchTermProp ?? '');
@@ -33,26 +45,79 @@ export const SearchSyncToolbar: FC<ISearchSyncToolbarProps> = ({
 
   return (
     <Toolbar {...rest}>
-      <Grid container alignItems="center" sx={{ columnGap: 2 }}>
-        <Grid item>
-          <SearchIcon color="inherit" sx={{ display: 'flex' }} />
-        </Grid>
-        <Grid item xs>
-          <TextField
-            fullWidth
-            placeholder={searchFieldPlaceholder}
-            InputProps={{
-              disableUnderline: true,
-              sx: { fontSize: 'default' },
-            }}
-            variant="standard"
-            value={searchTerm}
-            onChange={(event) => {
-              setSearchTerm(event.target.value);
-              onChangeSearchTerm && onChangeSearchTerm(event.target.value);
-            }}
-          />
-        </Grid>
+      <Grid container sx={{ alignItems: 'center', columnGap: 1 }}>
+        {title ? (
+          <>
+            <Grid item xs>
+              <Typography
+                {...titlePropsRest}
+                noWrap
+                sx={{
+                  fontWeight: 'bold',
+                  lineHeight: '48px',
+                  ...titlePropsSx,
+                }}
+              >
+                {title}
+              </Typography>
+            </Grid>
+            <Grid item sx={{ display: 'flex' }}>
+              {searchFieldOpen ? (
+                <ClickAwayListener
+                  onClickAway={() => {
+                    if (searchTerm.length <= 0) {
+                      setSearchFieldOpen(false);
+                    }
+                  }}
+                >
+                  <TextField
+                    placeholder={searchFieldPlaceholder}
+                    InputProps={{
+                      startAdornment: <SearchIcon color="inherit" />,
+                      sx: { fontSize: 'default' },
+                    }}
+                    variant="outlined"
+                    value={searchTerm}
+                    onChange={(event) => {
+                      setSearchTerm(event.target.value);
+                      onChangeSearchTerm &&
+                        onChangeSearchTerm(event.target.value);
+                    }}
+                    sx={{
+                      width: 200,
+                    }}
+                  />
+                </ClickAwayListener>
+              ) : (
+                <IconButton onClick={() => setSearchFieldOpen(true)}>
+                  <SearchIcon color="inherit" />
+                </IconButton>
+              )}
+            </Grid>
+          </>
+        ) : (
+          <>
+            <Grid item sx={{ display: 'flex' }}>
+              <SearchIcon color="inherit" />
+            </Grid>
+            <Grid item xs>
+              <TextField
+                fullWidth
+                placeholder={searchFieldPlaceholder}
+                InputProps={{
+                  disableUnderline: true,
+                  sx: { fontSize: 'default' },
+                }}
+                variant="standard"
+                value={searchTerm}
+                onChange={(event) => {
+                  setSearchTerm(event.target.value);
+                  onChangeSearchTerm && onChangeSearchTerm(event.target.value);
+                }}
+              />
+            </Grid>
+          </>
+        )}
         {(() => {
           if (tools) {
             const toolsList = Children.toArray(tools);
