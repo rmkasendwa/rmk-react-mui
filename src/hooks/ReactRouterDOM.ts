32ae1739e2196e466635f80917b26177c2899fa1
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2022-10-09 00:41:32 +0300

    Implements useReactRouterDOMSearchParams hook

--- src/hooks/ReactRouterDOM.ts
+++ src/hooks/ReactRouterDOM.ts
@@ -1,48 +1,13 @@
-import hash from 'object-hash';
-import { useCallback } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
+import { useSetSearchParam } from './Utils';
+
 export const useReactRouterDOMSearchParams = () => {
-  const [baseSearchParams, baseSetSearchParams] = useSearchParams();
-
-  const setSearchParams = useCallback(
-    (
-      searchParams: Record<string, string | null>,
-      navigateOptions?: {
-        replace?: boolean | undefined;
-        state?: any;
-      }
-    ) => {
-      const entries = new URL(window.location.href).searchParams.entries();
-      const existingSearchParams: Record<string, string> = {};
-      for (const [key, value] of entries) {
-        existingSearchParams[key] = value;
-      }
-      const combinedSearchParams = {
-        ...existingSearchParams,
-        ...searchParams,
-      };
-      const nextSearchParams = Object.keys(combinedSearchParams)
-        .filter((key) => {
-          return (
-            combinedSearchParams[key] != null &&
-            combinedSearchParams[key]!.length > 0
-          );
-        })
-        .reduce((accumulator, key) => {
-          accumulator[key] = combinedSearchParams[key]!;
-          return accumulator;
-        }, {} as Record<string, string>);
-
-      if (hash(nextSearchParams) !== hash(existingSearchParams)) {
-        baseSetSearchParams(nextSearchParams, navigateOptions);
-      }
-    },
-    [baseSetSearchParams]
-  );
+  const [searchParams, baseSetSearchParams] = useSearchParams();
+  const { setSearchParams } = useSetSearchParam(baseSetSearchParams);
 
   return {
-    searchParams: baseSearchParams,
+    searchParams,
     setSearchParams,
   };
 };
--- src/hooks/Utils.ts
+++ src/hooks/Utils.ts
@@ -1,5 +1,6 @@
 import { useMediaQuery, useTheme } from '@mui/material';
 import { pick } from 'lodash';
+import hash from 'object-hash';
 import {
   useCallback,
   useContext,
@@ -375,4 +376,52 @@ export const useAPIDataContext = () => {
 export const useSmallScreen = () => {
   const { breakpoints } = useTheme();
   return useMediaQuery(breakpoints.down('sm'));
+};
+
+export type SetSearchPrams = (
+  nextInit: Record<string, string>,
+  navigateOptions?: {
+    replace?: boolean | undefined;
+    state?: any;
+  }
+) => void;
+
+export const useSetSearchParam = (baseSetSearchParams: SetSearchPrams) => {
+  const setSearchParams = useCallback(
+    (
+      searchParams: Record<string, string | null>,
+      navigateOptions?: {
+        replace?: boolean | undefined;
+        state?: any;
+      }
+    ) => {
+      const entries = new URL(window.location.href).searchParams.entries();
+      const existingSearchParams: Record<string, string> = {};
+      for (const [key, value] of entries) {
+        existingSearchParams[key] = value;
+      }
+      const combinedSearchParams = {
+        ...existingSearchParams,
+        ...searchParams,
+      };
+      const nextSearchParams = Object.keys(combinedSearchParams)
+        .filter((key) => {
+          return (
+            combinedSearchParams[key] != null &&
+            combinedSearchParams[key]!.length > 0
+          );
+        })
+        .reduce((accumulator, key) => {
+          accumulator[key] = combinedSearchParams[key]!;
+          return accumulator;
+        }, {} as Record<string, string>);
+
+      if (hash(nextSearchParams) !== hash(existingSearchParams)) {
+        baseSetSearchParams(nextSearchParams, navigateOptions);
+      }
+    },
+    [baseSetSearchParams]
+  );
+
+  return { setSearchParams };
 };
