72061b82dc1d6a88907298f2e8ed933245580071
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2022-10-30 12:58:05 +0300

    Implements ReactRouterDomSearchParamCache

--- src/contexts/DataStoreContext.tsx
+++ src/contexts/DataStoreContext.tsx
@@ -3,21 +3,23 @@ import { createContext, useContext } from 'react';
 import { getMemorySize } from '../utils/data';
 import StorageManager from '../utils/StorageManager';
 
+const CACHED_DATA_PREFIX = 'cached-data';
 const MAX_DATA_MEMORY_SIZE = 15 * 1024; // 15KB
 const MAX_DATA_KEY_COUNT = 35;
-const allDataKeys: string[] = StorageManager.get('api-data-keys') || [];
+const allDataKeys: string[] =
+  StorageManager.get(`${CACHED_DATA_PREFIX}-keys`) || [];
 const dataKeys = allDataKeys.splice(-MAX_DATA_KEY_COUNT);
 const baseData: Record<string, any> = StorageManager.get('data') || {};
 
 if (allDataKeys.length > 0) {
   allDataKeys.forEach((key) => {
-    StorageManager.remove(`api-data-${key}`);
+    StorageManager.remove(`${CACHED_DATA_PREFIX}-${key}`);
   });
-  StorageManager.add(`api-data-keys`, dataKeys);
+  StorageManager.add(`${CACHED_DATA_PREFIX}-keys`, dataKeys);
 }
 
 dataKeys.forEach((key) => {
-  const keyData = StorageManager.get(`api-data-${key}`);
+  const keyData = StorageManager.get(`${CACHED_DATA_PREFIX}-${key}`);
   if (keyData) {
     Object.assign(baseData, {
       [key]: keyData,
@@ -35,7 +37,7 @@ export const DataStoreContext = createContext<DataStoreContext>({
     Object.assign(baseData, data);
     if (data && getMemorySize(data) <= MAX_DATA_MEMORY_SIZE) {
       Object.keys(data).forEach((key) => {
-        StorageManager.add(`api-data-${key}`, data[key]);
+        StorageManager.add(`${CACHED_DATA_PREFIX}-${key}`, data[key]);
         if (dataKeys.includes(key)) {
           dataKeys.splice(dataKeys.indexOf(key), 1);
         }
@@ -47,10 +49,10 @@ export const DataStoreContext = createContext<DataStoreContext>({
           dataKeys.length - MAX_DATA_KEY_COUNT
         );
         staleDataKeys.forEach((key) => {
-          StorageManager.remove(`api-data-${key}`);
+          StorageManager.remove(`${CACHED_DATA_PREFIX}-${key}`);
         });
       }
-      StorageManager.add(`api-data-keys`, dataKeys);
+      StorageManager.add(`${CACHED_DATA_PREFIX}-keys`, dataKeys);
     }
   },
 });
