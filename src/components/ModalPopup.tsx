765ef15a842fc07f85ee960dfb1b61cb6ebe6d42
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2022-11-01 03:23:15 +0300

    Improves ModalPopup customizability

--- src/components/ModalPopup.tsx
+++ src/components/ModalPopup.tsx
@@ -21,7 +21,14 @@ import {
   useThemeProps,
 } from '@mui/material';
 import clsx from 'clsx';
-import { Children, FC, ReactNode, useEffect, useRef, useState } from 'react';
+import {
+  Children,
+  ReactNode,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 
 import SearchSyncToolbar from './SearchSyncToolbar';
 
@@ -31,15 +38,6 @@ export interface ModalPopupClasses {
 }
 
 export type ModalPopupClassKey = keyof ModalPopupClasses;
-
-export function getModalPopupUtilityClass(slot: string): string {
-  return generateUtilityClass('MuiModalPopup', slot);
-}
-
-export const modalPopupClasses: ModalPopupClasses = generateUtilityClasses(
-  'MuiModalPopup',
-  ['root']
-);
 
 // Adding theme prop types
 declare module '@mui/material/styles/props' {
@@ -66,16 +64,6 @@ declare module '@mui/material/styles/components' {
   }
 }
 
-const useUtilityClasses = (ownerState: any) => {
-  const { classes } = ownerState;
-
-  const slots = {
-    root: ['root'],
-  };
-
-  return composeClasses(slots, getModalPopupUtilityClass, classes);
-};
-
 export interface ModalPopupProps extends Partial<Omit<ModalProps, 'children'>> {
   title: string;
   children?: ReactNode;
@@ -88,144 +76,176 @@ export interface ModalPopupProps extends Partial<Omit<ModalProps, 'children'>> {
   CloseActionButtonProps?: Partial<ButtonProps>;
 }
 
-export const ModalPopup: FC<ModalPopupProps> = (inProps) => {
-  const props = useThemeProps({ props: inProps, name: 'MuiModalPopup' });
-  const {
-    children,
-    title,
-    open,
-    onClose,
-    actionButtons,
-    CardProps = {},
-    loading = false,
-    CloseActionButtonProps = {},
-    sx,
-    ...rest
-  } = props;
+export function getModalPopupUtilityClass(slot: string): string {
+  return generateUtilityClass('MuiModalPopup', slot);
+}
 
-  const classes = useUtilityClasses({
-    ...props,
-  });
+export const modalPopupClasses: ModalPopupClasses = generateUtilityClasses(
+  'MuiModalPopup',
+  ['root']
+);
 
-  const { palette, components } = useTheme();
-
-  const { sx: cardPropsSx, ...cardPropsRest } = CardProps;
-  const {
-    children: closeActionButtonPropsChildren,
-    sx: closeActionButtonPropsSx,
-    ...closeActionButtonPropsRest
-  } = CloseActionButtonProps;
-
-  const onCloseRef = useRef(onClose);
-
-  useEffect(() => {
-    onCloseRef.current = onClose;
-  }, [onClose]);
-
-  const [detailsContainerElement, setDetailsContainerElement] =
-    useState<HTMLDivElement | null>(null);
-  const [, setDetailsContainerHeight] = useState(0);
-
-  useEffect(() => {
-    if (detailsContainerElement) {
-      const windowResizeEventCallback = () => {
-        setDetailsContainerHeight(detailsContainerElement.offsetHeight);
-      };
-      window.addEventListener('resize', windowResizeEventCallback);
-      setDetailsContainerHeight(detailsContainerElement.offsetHeight);
-      return () => {
-        window.removeEventListener('resize', windowResizeEventCallback);
-      };
-    }
-  }, [detailsContainerElement]);
-
-  return (
-    <Modal
-      {...rest}
-      className={clsx(classes.root)}
-      open={open}
-      onClose={(_, reason) => {
-        if (reason !== 'backdropClick' && onClose) {
-          onClose();
-        }
-      }}
-      sx={{
-        display: 'flex',
-        p: 4,
-        justifyContent: 'center',
-        alignItems: 'center',
-        ...(components?.MuiModalPopup?.styleOverrides?.root as any),
-        ...sx,
-      }}
-      disableEscapeKeyDown
-      disableAutoFocus
-    >
-      <Card
-        {...cardPropsRest}
-        sx={{ maxWidth: 640, width: `100%`, ...cardPropsSx }}
-      >
-        <SearchSyncToolbar
-          title={title}
-          hasSearchTool={false}
-          hasSyncTool={false}
-        >
-          {!loading ? (
-            <IconButton onClick={onClose} sx={{ p: 0.5 }}>
-              <CloseIcon />
-            </IconButton>
-          ) : null}
-        </SearchSyncToolbar>
-        <Divider />
-        <Box sx={{ py: 0, px: 3 }}>
-          {children ? (
-            <Box
-              ref={(detailsContainerElement: HTMLDivElement | null) => {
-                setDetailsContainerElement(detailsContainerElement);
-              }}
-              sx={{
-                py: 2,
-              }}
-            >
-              {children}
-            </Box>
-          ) : null}
-        </Box>
-        <Divider />
-        <Grid
-          container
-          spacing={2}
-          sx={{ py: 2, px: 3, flexDirection: 'row-reverse' }}
-        >
-          {(() => {
-            if (actionButtons) {
-              const toolsList = Children.toArray(actionButtons);
-              return toolsList.map((tool, index) => {
-                return (
-                  <Grid item key={index} sx={{ minWidth: 0 }}>
-                    {tool}
-                  </Grid>
-                );
-              });
-            }
-          })()}
-          <Grid item>
-            <Button
-              variant="outlined"
-              color="inherit"
-              {...closeActionButtonPropsRest}
-              onClick={onClose}
-              sx={{
-                color: alpha(palette.text.primary, 0.5),
-                ...closeActionButtonPropsSx,
-              }}
-            >
-              {closeActionButtonPropsChildren ?? 'Close'}
-            </Button>
-          </Grid>
-        </Grid>
-      </Card>
-    </Modal>
-  );
+const slots = {
+  root: ['root'],
 };
+
+export const ModalPopup = forwardRef<HTMLDivElement, ModalPopupProps>(
+  function ModalPopup(inProps, ref) {
+    const props = useThemeProps({ props: inProps, name: 'MuiModalPopup' });
+    const {
+      children,
+      title,
+      open,
+      onClose,
+      actionButtons,
+      CardProps = {},
+      loading = false,
+      CloseActionButtonProps = {},
+      sx,
+      className,
+      ...rest
+    } = props;
+
+    const classes = composeClasses(
+      slots,
+      getModalPopupUtilityClass,
+      (() => {
+        if (className) {
+          return {
+            root: className,
+          };
+        }
+      })()
+    );
+
+    const { palette, components } = useTheme();
+
+    const { sx: CardPropsSx, ...CardPropsRest } = CardProps;
+    const {
+      children: closeActionButtonPropsChildren,
+      sx: closeActionButtonPropsSx,
+      ...closeActionButtonPropsRest
+    } = CloseActionButtonProps;
+
+    // Refs
+    const onCloseRef = useRef(onClose);
+    useEffect(() => {
+      onCloseRef.current = onClose;
+    }, [onClose]);
+
+    const [detailsContainerElement, setDetailsContainerElement] =
+      useState<HTMLDivElement | null>(null);
+    const [, setDetailsContainerHeight] = useState(0);
+
+    useEffect(() => {
+      if (detailsContainerElement) {
+        const windowResizeEventCallback = () => {
+          setDetailsContainerHeight(detailsContainerElement.offsetHeight);
+        };
+        window.addEventListener('resize', windowResizeEventCallback);
+        setDetailsContainerHeight(detailsContainerElement.offsetHeight);
+        return () => {
+          window.removeEventListener('resize', windowResizeEventCallback);
+        };
+      }
+    }, [detailsContainerElement]);
+
+    return (
+      <Modal
+        {...rest}
+        ref={ref}
+        className={clsx(classes.root)}
+        open={open}
+        onClose={(_, reason) => {
+          if (reason !== 'backdropClick' && onClose) {
+            onClose();
+          }
+        }}
+        sx={{
+          display: 'flex',
+          p: 4,
+          justifyContent: 'center',
+          alignItems: 'center',
+          ...(components?.MuiModalPopup?.styleOverrides?.root as any),
+          ...sx,
+        }}
+        disableEscapeKeyDown
+        disableAutoFocus
+      >
+        <Card
+          {...CardPropsRest}
+          sx={{
+            maxWidth: 640,
+            width: `100%`,
+            maxHeight: `75%`,
+            display: 'flex',
+            flexDirection: 'column',
+            ...CardPropsSx,
+          }}
+        >
+          <SearchSyncToolbar
+            title={title}
+            hasSearchTool={false}
+            hasSyncTool={false}
+          >
+            {!loading ? (
+              <IconButton onClick={onClose} sx={{ p: 0.5 }}>
+                <CloseIcon />
+              </IconButton>
+            ) : null}
+          </SearchSyncToolbar>
+          <Divider />
+          <Box sx={{ py: 0, px: 3, overflowY: 'auto', flex: 1 }}>
+            {children ? (
+              <Box
+                ref={(detailsContainerElement: HTMLDivElement | null) => {
+                  setDetailsContainerElement(detailsContainerElement);
+                }}
+                sx={{
+                  py: 2,
+                }}
+              >
+                {children}
+              </Box>
+            ) : null}
+          </Box>
+          <Divider />
+          <Grid
+            container
+            spacing={2}
+            sx={{ py: 2, px: 3, flexDirection: 'row-reverse' }}
+          >
+            {(() => {
+              if (actionButtons) {
+                const toolsList = Children.toArray(actionButtons);
+                return toolsList.map((tool, index) => {
+                  return (
+                    <Grid item key={index} sx={{ minWidth: 0 }}>
+                      {tool}
+                    </Grid>
+                  );
+                });
+              }
+            })()}
+            <Grid item>
+              <Button
+                variant="outlined"
+                color="inherit"
+                {...closeActionButtonPropsRest}
+                onClick={onClose}
+                sx={{
+                  color: alpha(palette.text.primary, 0.5),
+                  ...closeActionButtonPropsSx,
+                }}
+              >
+                {closeActionButtonPropsChildren ?? 'Close'}
+              </Button>
+            </Grid>
+          </Grid>
+        </Card>
+      </Modal>
+    );
+  }
+);
 
 export default ModalPopup;
