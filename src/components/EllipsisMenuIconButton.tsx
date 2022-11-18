42cb077238e14ffb68f94dce4308eaa80cf276c8
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2022-11-19 02:33:39 +0300

    Refactors EllipsisMenuIconButton

--- src/components/EllipsisMenuIconButton.tsx
+++ src/components/EllipsisMenuIconButton.tsx
@@ -1,8 +1,22 @@
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { IconButton, IconButtonProps, Popper } from '@mui/material';
+import {
+  ComponentsOverrides,
+  ComponentsProps,
+  ComponentsVariants,
+  IconButton,
+  IconButtonProps,
+  Popper,
+  alpha,
+  unstable_composeClasses as composeClasses,
+  generateUtilityClass,
+  generateUtilityClasses,
+  useTheme,
+  useThemeProps,
+} from '@mui/material';
 import Box from '@mui/material/Box';
 import ClickAwayListener from '@mui/material/ClickAwayListener';
 import Grow from '@mui/material/Grow';
+import clsx from 'clsx';
 import { forwardRef, useRef, useState } from 'react';
 import { mergeRefs } from 'react-merge-refs';
 
@@ -13,6 +27,39 @@ import PaginatedDropdownOptionList, {
 
 export type { DropdownOption } from './PaginatedDropdownOptionList';
 
+export interface EllipsisMenuIconButtonClasses {
+  /** Styles applied to the root element. */
+  root: string;
+}
+
+export type EllipsisMenuIconButtonClassKey =
+  keyof EllipsisMenuIconButtonClasses;
+
+// Adding theme prop types
+declare module '@mui/material/styles/props' {
+  interface ComponentsPropsList {
+    MuiEllipsisMenuIconButton: EllipsisMenuIconButtonProps;
+  }
+}
+
+// Adding theme override types
+declare module '@mui/material/styles/overrides' {
+  interface ComponentNameToClassKey {
+    MuiEllipsisMenuIconButton: keyof EllipsisMenuIconButtonClasses;
+  }
+}
+
+// Adding theme component types
+declare module '@mui/material/styles/components' {
+  interface Components<Theme = unknown> {
+    MuiEllipsisMenuIconButton?: {
+      defaultProps?: ComponentsProps['MuiEllipsisMenuIconButton'];
+      styleOverrides?: ComponentsOverrides<Theme>['MuiEllipsisMenuIconButton'];
+      variants?: ComponentsVariants['MuiEllipsisMenuIconButton'];
+    };
+  }
+}
+
 export interface EllipsisMenuIconButtonProps
   extends IconButtonProps,
     EllipsisMenuToolProps {
@@ -20,20 +67,48 @@ export interface EllipsisMenuIconButtonProps
   closeOnSelectOption?: boolean;
 }
 
+export function getEllipsisMenuIconButtonUtilityClass(slot: string): string {
+  return generateUtilityClass('MuiEllipsisMenuIconButton', slot);
+}
+
+export const ellipsisMenuIconButtonClasses: EllipsisMenuIconButtonClasses =
+  generateUtilityClasses('MuiEllipsisMenuIconButton', ['root']);
+
+const slots = {
+  root: ['root'],
+};
+
 export const EllipsisMenuIconButton = forwardRef<
   HTMLButtonElement,
   EllipsisMenuIconButtonProps
->(function EllipsisMenuIconButton(
-  {
+>(function EllipsisMenuIconButton(inProps, ref) {
+  const props = useThemeProps({
+    props: inProps,
+    name: 'MuiEllipsisMenuIconButton',
+  });
+  const {
+    className,
     children = <MoreVertIcon />,
     options,
     PaginatedDropdownOptionListProps = {},
     closeOnSelectOption = true,
     ...rest
-  },
-  ref
-) {
+  } = props;
+
+  const classes = composeClasses(
+    slots,
+    getEllipsisMenuIconButtonUtilityClass,
+    (() => {
+      if (className) {
+        return {
+          root: className,
+        };
+      }
+    })()
+  );
+
   const anchorRef = useRef(null);
+  const { palette } = useTheme();
   const [open, setOpen] = useState(false);
 
   const close = () => setOpen(false);
@@ -43,8 +118,20 @@ export const EllipsisMenuIconButton = forwardRef<
       <IconButton
         {...rest}
         ref={mergeRefs([anchorRef, ref])}
+        className={clsx(classes.root)}
         onClick={() => {
           setOpen((prevOpen) => !prevOpen);
+        }}
+        sx={{
+          ...(() => {
+            if (open) {
+              return {
+                '&,&:hover': {
+                  bgcolor: alpha(palette.primary.main, 0.3),
+                },
+              };
+            }
+          })(),
         }}
       >
         {children}
@@ -54,17 +141,6 @@ export const EllipsisMenuIconButton = forwardRef<
         anchorEl={anchorRef.current}
         transition
         placement="bottom-end"
-        popperOptions={{
-          modifiers: [
-            {
-              name: 'offset',
-              options: {
-                enabled: true,
-                offset: [0, 12],
-              },
-            },
-          ],
-        }}
         sx={{ zIndex: 999 }}
       >
         {({ TransitionProps }) => {
