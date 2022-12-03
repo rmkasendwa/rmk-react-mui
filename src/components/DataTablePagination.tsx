6c87cf0269a6b6f524659a433817f8546442fe8c
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2022-12-03 03:11:17 +0300

    Refactors DataTablePagination

--- src/components/DataTablePagination.tsx
+++ src/components/DataTablePagination.tsx
@@ -1,10 +1,55 @@
-import { TablePaginationProps, inputBaseClasses } from '@mui/material';
+import {
+  ComponentsOverrides,
+  ComponentsProps,
+  ComponentsVariants,
+  TablePaginationProps,
+  unstable_composeClasses as composeClasses,
+  generateUtilityClass,
+  generateUtilityClasses,
+  inputBaseClasses,
+  useThemeProps,
+} from '@mui/material';
 import Grid, { GridProps } from '@mui/material/Grid';
 import Pagination, { PaginationProps } from '@mui/material/Pagination';
 import Typography from '@mui/material/Typography';
-import { forwardRef } from 'react';
+import clsx from 'clsx';
+import { omit } from 'lodash';
+import { forwardRef, useEffect, useState } from 'react';
+import { mergeRefs } from 'react-merge-refs';
 
 import DataDropdownField from './InputFields/DataDropdownField';
+
+export interface DataTablePaginationClasses {
+  /** Styles applied to the root element. */
+  root: string;
+}
+
+export type DataTablePaginationClassKey = keyof DataTablePaginationClasses;
+
+// Adding theme prop types
+declare module '@mui/material/styles/props' {
+  interface ComponentsPropsList {
+    MuiDataTablePagination: DataTablePaginationProps;
+  }
+}
+
+// Adding theme override types
+declare module '@mui/material/styles/overrides' {
+  interface ComponentNameToClassKey {
+    MuiDataTablePagination: keyof DataTablePaginationClasses;
+  }
+}
+
+// Adding theme component types
+declare module '@mui/material/styles/components' {
+  interface Components<Theme = unknown> {
+    MuiDataTablePagination?: {
+      defaultProps?: ComponentsProps['MuiDataTablePagination'];
+      styleOverrides?: ComponentsOverrides<Theme>['MuiDataTablePagination'];
+      variants?: ComponentsVariants['MuiDataTablePagination'];
+    };
+  }
+}
 
 export interface DataTablePaginationProps
   extends GridProps,
@@ -19,16 +64,30 @@ export interface DataTablePaginationProps
   PaginationProps?: Partial<PaginationProps>;
 }
 
+export function getDataTablePaginationUtilityClass(slot: string): string {
+  return generateUtilityClass('MuiDataTablePagination', slot);
+}
+
+export const dataTablePaginationClasses: DataTablePaginationClasses =
+  generateUtilityClasses('MuiDataTablePagination', ['root']);
+
+const slots = {
+  root: ['root'],
+};
+
 export const DataTablePagination = forwardRef<
   HTMLDivElement,
   DataTablePaginationProps
->(function DataTablePagination(
-  {
+>(function DataTablePagination(inProps, ref) {
+  const props = useThemeProps({
+    props: inProps,
+    name: 'MuiDataTablePagination',
+  });
+  const {
+    className,
     filteredCount,
     totalCount,
     labelPlural,
-    lowercaseLabelPlural,
-    labelSingular,
     page,
     PaginationProps,
     rowsPerPageOptions = [10, 25, 50, 100],
@@ -36,11 +95,40 @@ export const DataTablePagination = forwardRef<
     onRowsPerPageChange,
     sx,
     ...rest
-  },
-  ref
-) {
+  } = props;
+
+  let { labelSingular, lowercaseLabelPlural } = props;
+
+  const classes = composeClasses(
+    slots,
+    getDataTablePaginationUtilityClass,
+    (() => {
+      if (className) {
+        return {
+          root: className,
+        };
+      }
+    })()
+  );
+
   labelSingular ?? (labelSingular = labelPlural.replace(/s$/g, ''));
   lowercaseLabelPlural || (lowercaseLabelPlural = labelPlural.toLowerCase());
+
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    if (container) {
+      const windowResizeEventCallback = () => {
+        setIsSmallScreen(container.offsetWidth <= 960);
+      };
+      window.addEventListener('resize', windowResizeEventCallback);
+      windowResizeEventCallback();
+      return () => {
+        window.removeEventListener('resize', windowResizeEventCallback);
+      };
+    }
+  }, [container]);
 
   if (filteredCount <= 0) {
     return null;
@@ -48,8 +136,14 @@ export const DataTablePagination = forwardRef<
 
   return (
     <Grid
-      ref={ref}
-      {...rest}
+      ref={mergeRefs([
+        (container: HTMLDivElement) => {
+          setContainer(container);
+        },
+        ref,
+      ])}
+      className={clsx(classes.root)}
+      {...omit(rest, 'labelSingular', 'lowercaseLabelPlural')}
       container
       sx={{
         alignItems: 'center',
@@ -116,7 +210,13 @@ export const DataTablePagination = forwardRef<
         <Grid
           item
           sx={{
-            ml: 'auto',
+            ...(() => {
+              if (!isSmallScreen) {
+                return {
+                  ml: 'auto',
+                };
+              }
+            })(),
           }}
         >
           <Pagination
