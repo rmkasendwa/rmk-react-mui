4546f67de6193726006a268f27b2850a261ee45c
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2023-01-28 00:39:01 +0300

    Improves RichTextEditor implementation, Implements FormikRichTextEditor

---
+++ src/stories/RichTextEditor.stories.tsx
@@ -0,0 +1,25 @@
+import Container from '@mui/material/Container';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+
+import RichTextEditor, {
+  RichTextEditorProps,
+} from '../components/RichTextEditor';
+
+export default {
+  title: 'Components/Rich Text Editor',
+  component: RichTextEditor,
+} as ComponentMeta<typeof RichTextEditor>;
+
+const Template: ComponentStory<typeof RichTextEditor> = (props) => (
+  <Container maxWidth="lg" sx={{ p: 3 }}>
+    <RichTextEditor {...props} />
+  </Container>
+);
+
+export const Default = Template.bind({});
+
+export const WithValue = Template.bind({});
+WithValue.args = {
+  value: `<p>This is a paragaph</p><p>This is another <strong>paragraph</srong></p>`,
+} as RichTextEditorProps;
