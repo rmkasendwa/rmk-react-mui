46bb73730fa5db65cd8e895cbf2d049f7f573cfc
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2023-02-03 00:41:06 +0300

    Refactors FormWrapper component

--- src/components/FormWrapper.tsx
+++ src/components/FormWrapper.tsx
@@ -1,16 +1,59 @@
 import SaveIcon from '@mui/icons-material/Save';
 import LoadingButton from '@mui/lab/LoadingButton';
-import { useMediaQuery, useTheme } from '@mui/material';
+import {
+  ComponentsOverrides,
+  ComponentsProps,
+  ComponentsVariants,
+  unstable_composeClasses as composeClasses,
+  generateUtilityClass,
+  generateUtilityClasses,
+  useMediaQuery,
+  useTheme,
+  useThemeProps,
+} from '@mui/material';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import clsx from 'clsx';
 import { Form, Formik, FormikHelpers, FormikProps, FormikValues } from 'formik';
-import { FC, ReactNode } from 'react';
+import { Children, ReactNode, forwardRef } from 'react';
 
 import { useLoadingContext } from '../contexts/LoadingContext';
 import ErrorAlert from './ErrorAlert';
 import ErrorFieldHighlighter from './ErrorFieldHighlighter';
 import FixedHeaderContentArea from './FixedHeaderContentArea';
 import { PaddedContentAreaProps } from './PaddedContentArea';
+
+export interface FormWrapperClasses {
+  /** Styles applied to the root element. */
+  root: string;
+}
+
+export type FormWrapperClassKey = keyof FormWrapperClasses;
+
+// Adding theme prop types
+declare module '@mui/material/styles/props' {
+  interface ComponentsPropsList {
+    MuiFormWrapper: FormWrapperProps;
+  }
+}
+
+// Adding theme override types
+declare module '@mui/material/styles/overrides' {
+  interface ComponentNameToClassKey {
+    MuiFormWrapper: keyof FormWrapperClasses;
+  }
+}
+
+// Adding theme component types
+declare module '@mui/material/styles/components' {
+  interface Components<Theme = unknown> {
+    MuiFormWrapper?: {
+      defaultProps?: ComponentsProps['MuiFormWrapper'];
+      styleOverrides?: ComponentsOverrides<Theme>['MuiFormWrapper'];
+      variants?: ComponentsVariants['MuiFormWrapper'];
+    };
+  }
+}
 
 export interface FormWrapperProps<Values extends FormikValues = FormikValues>
   extends Omit<PaddedContentAreaProps, 'onSubmit' | 'children'> {
@@ -21,66 +64,118 @@ export interface FormWrapperProps<Values extends FormikValues = FormikValues>
     formikHelpers: FormikHelpers<Values>
   ) => void | Promise<any>;
   children?: ((props: FormikProps<Values>) => ReactNode) | ReactNode;
+  formTools?: ReactNode | ReactNode[];
 }
 
-export const FormWrapper: FC<FormWrapperProps> = ({
-  children,
-  title,
-  tools,
-  breadcrumbs,
-  initialValues = {},
-  validationSchema,
-  onSubmit,
-}) => {
-  const { breakpoints } = useTheme();
-  const smallScreen = useMediaQuery(breakpoints.down('sm'));
+export function getFormWrapperUtilityClass(slot: string): string {
+  return generateUtilityClass('MuiFormWrapper', slot);
+}
 
-  const { load, loading, errorMessage } = useLoadingContext();
+export const formWrapperClasses: FormWrapperClasses = generateUtilityClasses(
+  'MuiFormWrapper',
+  ['root']
+);
 
-  return (
-    <FixedHeaderContentArea {...{ title, breadcrumbs, tools }}>
-      {errorMessage && (
-        <Box sx={{ mb: 2 }}>
-          <ErrorAlert message={errorMessage} retry={load} />
-        </Box>
-      )}
-      <Formik
-        {...{ initialValues, validationSchema }}
-        onSubmit={async (values, formikHelpers) => {
-          onSubmit && (await onSubmit(values, formikHelpers));
-        }}
-        enableReinitialize
-      >
-        {({ isSubmitting, ...rest }) => {
-          return (
-            <Form noValidate>
-              <ErrorFieldHighlighter />
-              {typeof children === 'function'
-                ? children({ isSubmitting, ...rest })
-                : children}
-              {!loading && !errorMessage ? (
-                <Grid container sx={{ mt: 2 }}>
-                  {smallScreen ? null : <Grid item xs />}
-                  <Grid item xs={smallScreen}>
-                    <LoadingButton
-                      color="success"
-                      variant="contained"
-                      startIcon={<SaveIcon />}
-                      fullWidth={smallScreen}
-                      type="submit"
-                      loading={isSubmitting}
-                    >
-                      Save
-                    </LoadingButton>
-                  </Grid>
-                </Grid>
-              ) : null}
-            </Form>
-          );
-        }}
-      </Formik>
-    </FixedHeaderContentArea>
-  );
+const slots = {
+  root: ['root'],
 };
+
+export const FormWrapper = forwardRef<HTMLDivElement, FormWrapperProps>(
+  function FormWrapper(inProps, ref) {
+    const props = useThemeProps({ props: inProps, name: 'MuiFormWrapper' });
+    const {
+      className,
+      children,
+      title,
+      breadcrumbs,
+      initialValues = {},
+      validationSchema,
+      onSubmit,
+      tools,
+      formTools,
+      ...rest
+    } = props;
+
+    const classes = composeClasses(
+      slots,
+      getFormWrapperUtilityClass,
+      (() => {
+        if (className) {
+          return {
+            root: className,
+          };
+        }
+      })()
+    );
+
+    const { breakpoints } = useTheme();
+    const smallScreen = useMediaQuery(breakpoints.down('sm'));
+
+    const { load, loading, errorMessage } = useLoadingContext();
+
+    return (
+      <FixedHeaderContentArea
+        {...{ title, breadcrumbs, tools }}
+        ref={ref}
+        {...rest}
+        className={clsx(classes.root)}
+      >
+        {errorMessage && (
+          <Box sx={{ mb: 2 }}>
+            <ErrorAlert message={errorMessage} retry={load} />
+          </Box>
+        )}
+        <Formik
+          {...{ initialValues, validationSchema }}
+          onSubmit={async (values, formikHelpers) => {
+            onSubmit && (await onSubmit(values, formikHelpers));
+          }}
+          enableReinitialize
+        >
+          {({ isSubmitting, ...rest }) => {
+            return (
+              <Form noValidate>
+                <ErrorFieldHighlighter />
+                {typeof children === 'function'
+                  ? children({ isSubmitting, ...rest })
+                  : children}
+                {!loading && !errorMessage ? (
+                  <Grid container sx={{ mt: 2 }}>
+                    {smallScreen ? null : <Grid item xs />}
+                    {(() => {
+                      if (formTools) {
+                        return Children.toArray(formTools).map(
+                          (tool, index) => {
+                            return (
+                              <Grid item key={index} xs={smallScreen}>
+                                {tool}
+                              </Grid>
+                            );
+                          }
+                        );
+                      }
+                    })()}
+                    <Grid item xs={smallScreen}>
+                      <LoadingButton
+                        color="success"
+                        variant="contained"
+                        startIcon={<SaveIcon />}
+                        fullWidth={smallScreen}
+                        type="submit"
+                        loading={isSubmitting}
+                      >
+                        Save Changes
+                      </LoadingButton>
+                    </Grid>
+                  </Grid>
+                ) : null}
+              </Form>
+            );
+          }}
+        </Formik>
+      </FixedHeaderContentArea>
+    );
+  }
+);
 
 export default FormWrapper;
--- src/components/PaddedContentArea.tsx
+++ src/components/PaddedContentArea.tsx
@@ -1,27 +1,99 @@
+import {
+  ComponentsOverrides,
+  ComponentsProps,
+  ComponentsVariants,
+  unstable_composeClasses as composeClasses,
+  generateUtilityClass,
+  generateUtilityClasses,
+  useThemeProps,
+} from '@mui/material';
 import Box, { BoxProps } from '@mui/material/Box';
 import useTheme from '@mui/material/styles/useTheme';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { FC, ReactNode } from 'react';
+import clsx from 'clsx';
+import { ReactNode, forwardRef } from 'react';
 
 import PageTitle, { PageTitleProps } from './PageTitle';
 
+export interface PaddedContentAreaClasses {
+  /** Styles applied to the root element. */
+  root: string;
+}
+
+export type PaddedContentAreaClassKey = keyof PaddedContentAreaClasses;
+
+// Adding theme prop types
+declare module '@mui/material/styles/props' {
+  interface ComponentsPropsList {
+    MuiPaddedContentArea: PaddedContentAreaProps;
+  }
+}
+
+// Adding theme override types
+declare module '@mui/material/styles/overrides' {
+  interface ComponentNameToClassKey {
+    MuiPaddedContentArea: keyof PaddedContentAreaClasses;
+  }
+}
+
+// Adding theme component types
+declare module '@mui/material/styles/components' {
+  interface Components<Theme = unknown> {
+    MuiPaddedContentArea?: {
+      defaultProps?: ComponentsProps['MuiPaddedContentArea'];
+      styleOverrides?: ComponentsOverrides<Theme>['MuiPaddedContentArea'];
+      variants?: ComponentsVariants['MuiPaddedContentArea'];
+    };
+  }
+}
+
 export interface PaddedContentAreaProps
-  extends Pick<BoxProps, 'sx'>,
+  extends Pick<BoxProps, 'sx' | 'className'>,
     Pick<PageTitleProps, 'title' | 'tools'> {
   breadcrumbs?: ReactNode;
   PageTitleProps?: Partial<PageTitleProps>;
   children: ReactNode;
 }
 
-export const PaddedContentArea: FC<PaddedContentAreaProps> = ({
-  children,
-  title,
-  sx,
-  tools,
-  breadcrumbs,
-  PageTitleProps = {},
-  ...rest
-}) => {
+export function getPaddedContentAreaUtilityClass(slot: string): string {
+  return generateUtilityClass('MuiPaddedContentArea', slot);
+}
+
+export const paddedContentAreaClasses: PaddedContentAreaClasses =
+  generateUtilityClasses('MuiPaddedContentArea', ['root']);
+
+const slots = {
+  root: ['root'],
+};
+
+export const PaddedContentArea = forwardRef<
+  HTMLDivElement,
+  PaddedContentAreaProps
+>(function PaddedContentArea(inProps, ref) {
+  const props = useThemeProps({ props: inProps, name: 'MuiPaddedContentArea' });
+  const {
+    className,
+    children,
+    title,
+    sx,
+    tools,
+    breadcrumbs,
+    PageTitleProps = {},
+    ...rest
+  } = props;
+
+  const classes = composeClasses(
+    slots,
+    getPaddedContentAreaUtilityClass,
+    (() => {
+      if (className) {
+        return {
+          root: className,
+        };
+      }
+    })()
+  );
+
   const { sx: PageTitlePropsSx, ...PageTitlePropsRest } = PageTitleProps;
   const { breakpoints } = useTheme();
   const smallScreenBreakpoint = breakpoints.down('sm');
@@ -30,6 +102,8 @@ export const PaddedContentArea: FC<PaddedContentAreaProps> = ({
   return (
     <Box
       {...rest}
+      ref={ref}
+      className={clsx(classes.root)}
       sx={{
         flex: 1,
         px: 3,
@@ -53,6 +127,7 @@ export const PaddedContentArea: FC<PaddedContentAreaProps> = ({
       {children}
     </Box>
   );
-};
+  return <Box></Box>;
+});
 
 export default PaddedContentArea;
