a6d0ea7aaf7eb7ba90837cf61f05cf0295044a42
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2023-02-13 02:19:46 +0300

    Improves link implementation on RichTextEditor component

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -124,6 +124,62 @@ export const createLinkDecorator = () =>
     },
   ]);
 
+export const getEntityAtOffset = (block: ContentBlock, offset: number) => {
+  const entityKey = block.getEntityAt(offset);
+  if (entityKey == null) {
+    return null;
+  }
+  let startOffset = offset;
+  while (startOffset > 0 && block.getEntityAt(startOffset - 1) === entityKey) {
+    startOffset -= 1;
+  }
+  let endOffset = startOffset;
+  const blockLength = block.getLength();
+  while (
+    endOffset < blockLength &&
+    block.getEntityAt(endOffset + 1) === entityKey
+  ) {
+    endOffset += 1;
+  }
+  return {
+    entityKey,
+    blockKey: block.getKey(),
+    startOffset,
+    endOffset: endOffset + 1,
+  };
+};
+
+export const getEntityAtCursor = (editorState: EditorState) => {
+  const selection = editorState.getSelection();
+  const startKey = selection.getStartKey();
+  const startBlock = editorState.getCurrentContent().getBlockForKey(startKey);
+  const startOffset = selection.getStartOffset();
+  if (selection.isCollapsed()) {
+    // Get the entity before the cursor (unless the cursor is at the start).
+    return getEntityAtOffset(
+      startBlock,
+      startOffset === 0 ? startOffset : startOffset - 1
+    );
+  }
+  if (startKey !== selection.getEndKey()) {
+    return null;
+  }
+  const endOffset = selection.getEndOffset();
+  const startEntityKey = startBlock.getEntityAt(startOffset);
+  for (let i = startOffset; i < endOffset; i++) {
+    const entityKey = startBlock.getEntityAt(i);
+    if (entityKey == null || entityKey !== startEntityKey) {
+      return null;
+    }
+  }
+  return {
+    entityKey: startEntityKey,
+    blockKey: startBlock.getKey(),
+    startOffset: startOffset,
+    endOffset: endOffset,
+  };
+};
+
 export type RichTextEditorTools = Record<
   | 'ALIGN_CENTER'
   | 'ALIGN_LEFT'
@@ -313,13 +369,24 @@ export const RichTextEditor = forwardRef<HTMLDivElement, RichTextEditorProps>(
       return EditorState.createEmpty();
     });
     const selectionState = editorState.getSelection();
+    const contentState = editorState.getCurrentContent();
     const currentInlineStyle = editorState.getCurrentInlineStyle();
-    const blockType = editorState
-      .getCurrentContent()
+    const blockType = contentState
       .getBlockForKey(selectionState.getStartKey())
       .getType();
 
     const hasFocus = selectionState.getHasFocus();
+    const entityAtCursor = getEntityAtCursor(editorState);
+    const isCursorOnLink = Boolean(
+      (() => {
+        if (entityAtCursor != null) {
+          return (
+            contentState.getEntity(entityAtCursor.entityKey).getType() ===
+            'LINK'
+          );
+        }
+      })()
+    );
 
     const [textAlignment, setTextAlignment] =
       useState<DraftTextAlignment>('left');
@@ -382,6 +449,7 @@ export const RichTextEditor = forwardRef<HTMLDivElement, RichTextEditorProps>(
           onMouseDown: () => {
             setAddLinkModalOpen(true);
           },
+          isActive: isCursorOnLink,
           label: 'Link',
           id: 'LINK',
         },
