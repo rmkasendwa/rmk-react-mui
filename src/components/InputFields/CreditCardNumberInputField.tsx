5862e5011acedf34fcd812a78fa3a11010c2415a
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2023-03-06 20:06:53 +0300

    Refactors credit card input fields

--- src/components/InputFields/CreditCardNumberInputField.tsx
+++ src/components/InputFields/CreditCardNumberInputField.tsx
@@ -1,7 +1,19 @@
-import { forwardRef, useState } from 'react';
+import { forwardRef, useCallback, useEffect, useRef, useState } from 'react';
 
 import CreditCardIcon from '../Icons/CreditCardIcon';
 import TextField, { TextFieldProps } from './TextField';
+
+export const getValidInputValue = (inputValue: string) => {
+  const numericDigitsMatch = inputValue.match(/\d/g);
+  if (numericDigitsMatch) {
+    return numericDigitsMatch
+      .join('')
+      .slice(0, 20)
+      .match(/\d{1,4}/g)!
+      .join(' ');
+  }
+  return '';
+};
 
 export interface CreditCardNumberInputFieldProps
   extends Omit<TextFieldProps, 'value'> {
@@ -12,29 +24,53 @@ export const CreditCardNumberInputField = forwardRef<
   HTMLDivElement,
   CreditCardNumberInputFieldProps
 >(function CreditCardNumberInputField(
-  { name, id, disabled, endAdornment, ...rest },
+  { name, id, endAdornment, value, onChange, ...rest },
   ref
 ) {
+  // Refs
+  const onChangeRef = useRef(onChange);
+  useEffect(() => {
+    onChangeRef.current = onChange;
+  }, [onChange]);
+
+  const triggerChangeEvent = useCallback(
+    (inputValue: string) => {
+      if (onChangeRef.current) {
+        const event: any = new Event('change', { bubbles: true });
+        Object.defineProperty(event, 'target', {
+          writable: false,
+          value: {
+            name,
+            id,
+            value: inputValue,
+          },
+        });
+        onChangeRef.current(event);
+      }
+    },
+    [id, name]
+  );
+
   const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    if (onChangeRef.current && value != null) {
+      setInputValue(getValidInputValue(value));
+    }
+  }, [value]);
+
   return (
     <TextField
       ref={ref}
       placeholder="1234 5678 9012 3456"
       {...rest}
-      {...{ name, id, disabled }}
+      {...{ name, id }}
       onChange={(event) => {
-        const numericDigitsMatch = String(event.target.value).match(/\d/g);
-        if (numericDigitsMatch) {
-          setInputValue(
-            numericDigitsMatch
-              .join('')
-              .slice(0, 20)
-              .match(/\d{1,4}/g)!
-              .join(' ')
-          );
-        } else {
-          setInputValue('');
+        const nextInputValue = getValidInputValue(String(event.target.value));
+        if (!onChangeRef.current || value == null) {
+          setInputValue(nextInputValue);
         }
+        triggerChangeEvent(nextInputValue);
       }}
       value={inputValue}
       endAdornment={
