d3f6ca797335d4d03b8a498b3d4c6f863209cb8b
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2023-03-06 19:42:54 +0300

    Implements CreditCardCVCInputField component

---
+++ src/stories/InputFields/CreditCardCVCInputField.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+
+import { CreditCardCVCInputField } from '../../components/InputFields/CreditCardCVCInputField';
+
+export default {
+  title: 'Components/Input Fields/Credit Card CVC Input Field',
+  component: CreditCardCVCInputField,
+  parameters: {
+    layout: 'centered',
+  },
+} as ComponentMeta<typeof CreditCardCVCInputField>;
+
+const Template: ComponentStory<typeof CreditCardCVCInputField> = (props) => {
+  return (
+    <CreditCardCVCInputField
+      label="CVC / CVV"
+      {...props}
+      sx={{ minWidth: 300 }}
+    />
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  required: true,
+};
