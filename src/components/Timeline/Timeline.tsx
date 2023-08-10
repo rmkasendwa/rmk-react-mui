b47978795d840bd1e3bb132ba15c811b493a0dde
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2023-08-10 17:43:45 +0300

    Detaches useTimelineDataComputedProperties hook from Timeline component

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -22,7 +22,6 @@ import {
   useThemeProps,
 } from '@mui/material';
 import clsx from 'clsx';
-import addDays from 'date-fns/addDays';
 import addHours from 'date-fns/addHours';
 import differenceInDays from 'date-fns/differenceInDays';
 import differenceInHours from 'date-fns/differenceInHours';
@@ -59,6 +58,7 @@ import {
   useScrollTimelineTools,
   useTimeScaleTool,
 } from './hooks';
+import { useTimelineDataComputedProperties } from './hooks/TimelineDataComputedProperties';
 import { useTimeScaleMeterConfiguration } from './hooks/TimeScaleMeterConfiguration';
 import {
   ScrollToDateFunction,
@@ -454,9 +454,6 @@ export const BaseTimeline = <RecordRow extends BaseDataRow>(
   const getDefaultViewResetFunctionRef = useRef(getDefaultViewResetFunction);
   getDefaultViewResetFunctionRef.current = getDefaultViewResetFunction;
 
-  const getTimelineDatesRef = useRef(getTimelineDates);
-  getTimelineDatesRef.current = getTimelineDates;
-
   const onChangeSelectedTimeScaleRef = useRef(onChangeSelectedTimeScale);
   onChangeSelectedTimeScaleRef.current = onChangeSelectedTimeScale;
 
@@ -546,141 +543,23 @@ export const BaseTimeline = <RecordRow extends BaseDataRow>(
    * Memoized calculation of various timeline-related values and properties.
    */
   const {
-    minCalendarDate, // The minimum date in the calendar based on the timeline data.
-    maxCalendarDate, // The maximum date in the calendar based on the timeline data.
-    timelineYears, // An array of years represented in the timeline data.
-    totalNumberOfDays, // The total number of days covered by the timeline.
-    totalNumberOfHours, // The total number of hours covered by the timeline.
-    centerOfGravity, // The calculated center of gravity date based on the timeline data.
-    allDates, // An array containing all the dates extracted from the timeline data.
-    timelineDifferenceInDays, // The difference in days between the maximum and minimum dates in the timeline data.
-    timelineDifferenceInHours, // The difference in hours between the maximum and minimum dates in the timeline data.
-  } = useMemo(() => {
-    // A function to get all dates from the timeline data using the getTimelineDatesRef.current function.
-    const allDates = (() => {
-      if (getTimelineDatesRef.current) {
-        const dates = getTimelineDatesRef.current(rows);
-        // Converting each date to the format without timezone offset.
-        return dates.map((date) => {
-          return createDateWithoutTimezoneOffset(date);
-        });
-      }
-      // If getTimelineDatesRef.current is not provided, manually extract dates from the rows.
-      return rows.flatMap((row) => {
-        const dates: Date[] = [];
-        const startDateValue = (() => {
-          // Extracting the start date from each row based on the startDateProperty.
-          if (startDateProperty) {
-            return result(row, startDateProperty);
-          }
-        })();
-        if (startDateValue) {
-          // Handling different date formats for the start date and adding to the dates array.
-          if (startDateValue instanceof Date) {
-            dates.push(startDateValue);
-          } else {
-            const parsedStartDateValue = createDateWithoutTimezoneOffset(
-              startDateValue as any
-            );
-            if (!isNaN(parsedStartDateValue.getTime())) {
-              dates.push(parsedStartDateValue);
-            }
-          }
-        }
-        const endDateValue = (() => {
-          // Extracting the end date from each row based on the endDateProperty.
-          if (endDateProperty) {
-            return result(row, endDateProperty);
-          }
-        })();
-        if (endDateValue) {
-          // Handling different date formats for the end date and adding to the dates array.
-          if (endDateValue instanceof Date) {
-            dates.push(endDateValue);
-          } else {
-            const parsedEndDateValue = createDateWithoutTimezoneOffset(
-              endDateValue as any
-            );
-            if (!isNaN(parsedEndDateValue.getTime())) {
-              dates.push(parsedEndDateValue);
-            }
-          }
-        }
-        return dates;
-      });
-    })().sort((a, b) => a.getTime() - b.getTime());
-
-    // Calculate the minimum and maximum dates and calendar dates based on the extracted dates.
-    const { maxDate, minDate, maxCalendarDate, minCalendarDate } = (() => {
-      if (allDates.length > 0) {
-        const minDate = minDateProp
-          ? createDateWithoutTimezoneOffset(minDateProp)
-          : allDates[0];
-        const minCalendarDate = new Date(minDate.getFullYear(), 0, 1, 0, 0);
-
-        const maxDate = maxDateProp
-          ? createDateWithoutTimezoneOffset(maxDateProp)
-          : allDates[allDates.length - 1];
-        const maxCalendarDate = new Date(maxDate.getFullYear(), 11, 31, 23, 59);
-
-        return {
-          minDate,
-          maxDate,
-          minCalendarDate,
-          maxCalendarDate,
-        };
-      }
-      // If no dates are extracted, set the minimum and maximum dates to the current year.
-      const thisYear = new Date().getFullYear();
-      const minDate = new Date(thisYear, 0, 1, 0, 0);
-      const maxDate = new Date(thisYear, 11, 31, 23, 59);
-      return {
-        minDate,
-        maxDate,
-        minCalendarDate: minDate,
-        maxCalendarDate: maxDate,
-      };
-    })();
-
-    // Calculate the years represented in the timeline data and add them to the timelineYears array.
-    const minDateYear = minCalendarDate.getFullYear();
-    const maxDateYear = maxCalendarDate.getFullYear();
-    const timelineYears: number[] = [];
-    for (let year = minDateYear; year <= maxDateYear; year++) {
-      timelineYears.push(year);
-    }
-
-    // Calculate totalNumberOfDays and totalNumberOfHours covered by the timeline.
-    const totalNumberOfDays =
-      differenceInDays(maxCalendarDate, minCalendarDate) + 1;
-    const totalNumberOfHours =
-      differenceInHours(maxCalendarDate, minCalendarDate) + 1;
-
-    // Calculate timelineDifferenceInDays and timelineDifferenceInHours.
-    const timelineDifferenceInDays = differenceInDays(maxDate, minDate);
-    const timelineDifferenceInHours = differenceInHours(maxDate, minDate);
-
-    // Calculate centerOfGravity based on the extracted dates.
-    const centerOfGravity =
-      allDates.length > 0
-        ? addDays(minDate, Math.floor(timelineDifferenceInDays / 2))
-        : new Date();
-
-    // Return all the calculated values as an object.
-    return {
-      minDate,
-      maxDate,
-      minCalendarDate,
-      maxCalendarDate,
-      timelineYears,
-      totalNumberOfDays,
-      totalNumberOfHours,
-      timelineDifferenceInDays,
-      timelineDifferenceInHours,
-      centerOfGravity,
-      allDates,
-    };
-  }, [endDateProperty, maxDateProp, minDateProp, rows, startDateProperty]);
+    minCalendarDate,
+    maxCalendarDate,
+    timelineYears,
+    totalNumberOfDays,
+    totalNumberOfHours,
+    centerOfGravity,
+    allDates,
+    timelineDifferenceInDays,
+    timelineDifferenceInHours,
+  } = useTimelineDataComputedProperties({
+    rows,
+    endDateProperty,
+    startDateProperty,
+    minDate: minDateProp,
+    maxDate: maxDateProp,
+    getTimelineDates,
+  });
 
   minCalendarDateRef && (minCalendarDateRef.current = minCalendarDate);
   maxCalendarDateRef && (maxCalendarDateRef.current = maxCalendarDate);
