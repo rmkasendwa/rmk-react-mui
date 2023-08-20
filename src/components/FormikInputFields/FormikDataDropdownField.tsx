c274b1f3e57cb73db2d44b34464a15ea00e5602f
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2023-08-21 01:00:20 +0300

    Refactors Formik input fields

--- src/components/FormikInputFields/FormikDataDropdownField.tsx
+++ src/components/FormikInputFields/FormikDataDropdownField.tsx
@@ -1,3 +1,13 @@
+import {
+  ComponentsOverrides,
+  ComponentsProps,
+  ComponentsVariants,
+  unstable_composeClasses as composeClasses,
+  generateUtilityClass,
+  generateUtilityClasses,
+  useThemeProps,
+} from '@mui/material';
+import clsx from 'clsx';
 import { ReactElement, Ref, forwardRef } from 'react';
 
 import { useAggregatedFormikContext } from '../../hooks/Formik';
@@ -5,11 +15,72 @@ import DataDropdownField, {
   DataDropdownFieldProps,
 } from '../InputFields/DataDropdownField';
 
+export interface FormikDataDropdownFieldClasses {
+  /** Styles applied to the root element. */
+  root: string;
+}
+
+export type FormikDataDropdownFieldClassKey =
+  keyof FormikDataDropdownFieldClasses;
+
+//#region Adding theme prop types
+declare module '@mui/material/styles/props' {
+  interface ComponentsPropsList {
+    MuiFormikDataDropdownField: FormikDataDropdownFieldProps;
+  }
+}
+//#endregion
+
+//#region Adding theme override types
+declare module '@mui/material/styles/overrides' {
+  interface ComponentNameToClassKey {
+    MuiFormikDataDropdownField: keyof FormikDataDropdownFieldClasses;
+  }
+}
+//#endregion
+
+//#region Adding theme component types
+declare module '@mui/material/styles/components' {
+  interface Components<Theme = unknown> {
+    MuiFormikDataDropdownField?: {
+      defaultProps?: ComponentsProps['MuiFormikDataDropdownField'];
+      styleOverrides?: ComponentsOverrides<Theme>['MuiFormikDataDropdownField'];
+      variants?: ComponentsVariants['MuiFormikDataDropdownField'];
+    };
+  }
+}
+//#endregion
+
+export const getFormikDataDropdownFieldUtilityClass = (slot: string) => {
+  return generateUtilityClass('MuiFormikDataDropdownField', slot);
+};
+
+const slots: Record<
+  FormikDataDropdownFieldClassKey,
+  [FormikDataDropdownFieldClassKey]
+> = {
+  root: ['root'],
+};
+
+export const formikDataDropdownFieldClasses: FormikDataDropdownFieldClasses =
+  generateUtilityClasses(
+    'MuiFormikDataDropdownField',
+    Object.keys(slots) as FormikDataDropdownFieldClassKey[]
+  );
+
 export interface FormikDataDropdownFieldProps<Entity = any>
   extends DataDropdownFieldProps<Entity> {}
 
-const BaseFormikDataDropdownField = <Entity,>(
-  {
+export const BaseFormikDataDropdownField = <Entity,>(
+  inProps: FormikDataDropdownFieldProps<Entity>,
+  ref: Ref<HTMLDivElement>
+) => {
+  const props = useThemeProps({
+    props: inProps,
+    name: 'MuiFormikDataDropdownField',
+  });
+  const {
+    className,
     name,
     value: valueProp,
     onBlur: onBlurProp,
@@ -17,9 +88,20 @@ const BaseFormikDataDropdownField = <Entity,>(
     error: errorProp,
     helperText: helperTextProp,
     ...rest
-  }: DataDropdownFieldProps<Entity>,
-  ref: Ref<HTMLDivElement>
-) => {
+  } = props;
+
+  const classes = composeClasses(
+    slots,
+    getFormikDataDropdownFieldUtilityClass,
+    (() => {
+      if (className) {
+        return {
+          root: className,
+        };
+      }
+    })()
+  );
+
   const { value, onChange, onBlur, error, helperText } =
     useAggregatedFormikContext({
       value: valueProp,
@@ -34,6 +116,7 @@ const BaseFormikDataDropdownField = <Entity,>(
     <DataDropdownField
       ref={ref}
       {...rest}
+      className={clsx(classes.root)}
       {...({ name, value, onChange, onBlur, error, helperText } as any)}
     />
   );
--- src/components/FormikInputFields/FormikDateInputField.tsx
+++ src/components/FormikInputFields/FormikDateInputField.tsx
@@ -1,3 +1,13 @@
+import {
+  ComponentsOverrides,
+  ComponentsProps,
+  ComponentsVariants,
+  unstable_composeClasses as composeClasses,
+  generateUtilityClass,
+  generateUtilityClasses,
+  useThemeProps,
+} from '@mui/material';
+import clsx from 'clsx';
 import { forwardRef } from 'react';
 
 import { useAggregatedFormikContext } from '../../hooks/Formik';
@@ -5,13 +15,70 @@ import DateInputField, {
   DateInputFieldProps,
 } from '../InputFields/DateInputField';
 
+export interface FormikDateInputFieldClasses {
+  /** Styles applied to the root element. */
+  root: string;
+}
+
+export type FormikDateInputFieldClassKey = keyof FormikDateInputFieldClasses;
+
+//#region Adding theme prop types
+declare module '@mui/material/styles/props' {
+  interface ComponentsPropsList {
+    MuiFormikDateInputField: FormikDateInputFieldProps;
+  }
+}
+//#endregion
+
+//#region Adding theme override types
+declare module '@mui/material/styles/overrides' {
+  interface ComponentNameToClassKey {
+    MuiFormikDateInputField: keyof FormikDateInputFieldClasses;
+  }
+}
+//#endregion
+
+//#region Adding theme component types
+declare module '@mui/material/styles/components' {
+  interface Components<Theme = unknown> {
+    MuiFormikDateInputField?: {
+      defaultProps?: ComponentsProps['MuiFormikDateInputField'];
+      styleOverrides?: ComponentsOverrides<Theme>['MuiFormikDateInputField'];
+      variants?: ComponentsVariants['MuiFormikDateInputField'];
+    };
+  }
+}
+//#endregion
+
+export const getFormikDateInputFieldUtilityClass = (slot: string) => {
+  return generateUtilityClass('MuiFormikDateInputField', slot);
+};
+
+const slots: Record<
+  FormikDateInputFieldClassKey,
+  [FormikDateInputFieldClassKey]
+> = {
+  root: ['root'],
+};
+
+export const formikDateInputFieldClasses: FormikDateInputFieldClasses =
+  generateUtilityClasses(
+    'MuiFormikDateInputField',
+    Object.keys(slots) as FormikDateInputFieldClassKey[]
+  );
+
 export interface FormikDateInputFieldProps extends DateInputFieldProps {}
 
 export const FormikDateInputField = forwardRef<
   HTMLDivElement,
   FormikDateInputFieldProps
->(function FormikDateInputField(
-  {
+>(function FormikDateInputField(inProps, ref) {
+  const props = useThemeProps({
+    props: inProps,
+    name: 'MuiFormikDateInputField',
+  });
+  const {
+    className,
     name,
     value: valueProp,
     onBlur: onBlurProp,
@@ -19,9 +86,20 @@ export const FormikDateInputField = forwardRef<
     error: errorProp,
     helperText: helperTextProp,
     ...rest
-  },
-  ref
-) {
+  } = props;
+
+  const classes = composeClasses(
+    slots,
+    getFormikDateInputFieldUtilityClass,
+    (() => {
+      if (className) {
+        return {
+          root: className,
+        };
+      }
+    })()
+  );
+
   const { value, onChange, onBlur, error, helperText } =
     useAggregatedFormikContext({
       value: valueProp,
@@ -36,6 +114,7 @@ export const FormikDateInputField = forwardRef<
     <DateInputField
       ref={ref}
       {...rest}
+      className={clsx(classes.root)}
       {...({ name, value, onChange, onBlur, error, helperText } as any)}
     />
   );
